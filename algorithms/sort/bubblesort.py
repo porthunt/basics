117b6af01db1005dc84cb522bd20b36172a361cc
porthunt <jportela.gomez@gmail.com>
2018-09-03 22:56:38 +0100

    changes on bubblesort

--- algorithms/sort/bubblesort.py
+++ algorithms/sort/bubblesort.py
@@ -1,9 +1,5 @@
-"""
-A collection of multiple sorting algorithms.
-"""
 
-
-def bubblesort(S):
+def bubblesort(a_list):
     """
     The Bubble sort algorithm is the most naive one that we can create. The idea around this algorithm
     is comparing each two elements on the list and swapping them in case one is bigger than the other.
@@ -25,11 +21,12 @@ def bubblesort(S):
         2nd round: [2, 1, 3, 4]
         3rd round: [1, 2, 3, 4]
         So we can see that its the number of elements (N) times the number of swaps (N-1). N^2-1 rounds.
+    :param a_list: a list with numbers.
     :return: a list with the elements sorted (asc -> desc).
     """
     updated = False
-    for i in range(0, len(S)-1):
-        if S[i] > S[i+1]:
-            S[i+1], S[i] = S[i], S[i+1]
+    for i in range(0, len(a_list) - 1):
+        if a_list[i] > a_list[i + 1]:
+            a_list[i + 1], a_list[i] = a_list[i], a_list[i + 1]
             updated = True
-    return S if not updated else bubblesort(S)
+    return a_list if not updated else bubblesort(a_list)
